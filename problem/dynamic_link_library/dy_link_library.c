55bddda65acfb7eb333b541e273f3a7ce5d49e43
lougengle <596915985@qq.com>
2021-07-12 16:44:11 +0800

    补全动态接口

--- problem/dynamic_link_library/dy_link_library.c
+++ problem/dynamic_link_library/dy_link_library.c
@@ -8,6 +8,31 @@ dy_link_library::~dy_link_library()
 {
 }
 
+bool dy_link_library::init()
+{
+    cout << "init" << endl;
+
+    return true;
+}
+
+bool dy_link_library::exit()
+{
+    cout << "exit" << endl;
+    return true;
+}
+
+bool dy_link_library::start()
+{
+    cout << "start" << endl;
+    return true;
+}
+
+bool dy_link_library::stop()
+{
+    cout << "stop" << endl;
+    return true;
+}
+
 dy_link_library::int()
 {
     
