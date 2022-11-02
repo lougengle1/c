879a85a4a59d617cdfd2182a5f6173834a363c26
lougengle1 <44103655+lougengle1@users.noreply.github.com>
2022-11-02 17:07:03 +0800

    Delete driver/slnic_pub_driver directory

--- driver/slnic_pub_driver/control_cdev.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROL_CDEV_H_20160523
-#define CONTROL_CDEV_H_20160523
-
-#include <linux/cdev.h>
-#include <linux/fs.h>
-
-extern struct file_operations g_fop_control;
-
-#endif
--- driver/slnic_pub_driver/slk_tools/slk_4_14_x.h
+++
@@ -1,7 +0,0 @@
-#ifndef SLK_4_14_X_H
-#define SLK_4_14_X_H
-
-#define copy_from_user(to,from,len) raw_copy_from_user((to),(from),(len))
-#define copy_to_user(to,from,len) raw_copy_to_user((to),(from),(len))
-
-#endif
